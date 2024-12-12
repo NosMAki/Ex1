833526d5c24b1cb895892a17241484bc121f02df
NosMAki <saar0106@gmail.com>
2024-12-12 14:50:39 +0200

    sixth commit - main is done and Ex1 is done only a few tests are left to be  added

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,7 +1,7 @@
 public class Ex1 {
 
     public static void main(String[] args) {
-        if (isNumber("AAbB")) {
+        if (isNumber("10b")) {
 
             System.out.println("True");
         } else {
@@ -11,7 +11,6 @@ public class Ex1 {
 
 
     /**
-     *
      * @param Input
      * @return
      */
@@ -19,26 +18,36 @@ public class Ex1 {
         int ans = -1;
 
         // checking if the number is valid by running it through the isNumber function
-        if (!isNumber(Input)){
+        if (!isNumber(Input)) {
             return ans;
         }
         // making an index again to split the string into 2 substrings
         int indexB = Input.indexOf('b');
 
-        String numberPart = Input.substring(0,indexB);
-        String basePart = Input.substring(indexB+ 1);
+        if (indexB == -1){
+            //
+            try {
+                ans = Integer.parseInt(Input);
+            } catch (NumberFormatException e){
+                return -1;
+            }
+            return ans;
+        }
 
+        String numberPart = Input.substring(0, indexB);
+        String basePart = Input.substring(indexB + 1);
 
 
         // convert the basePart into an int
-        int base = Character.digit(basePart.charAt(0) , 17); // convert the base into the int
-        if (base < 2 || base > 16){
+        int base = Character.digit(basePart.charAt(0), 17); // convert the base into the int
+        if (base < 2 || base > 16) {
             return ans;
         }
 
 
         // we use parseInt to convert the numberPart to a decimal int
-        try { ans = Integer.parseInt(numberPart , base);
+        try {
+            ans = Integer.parseInt(numberPart, base);
         } catch (NumberFormatException e) {
             return -1;
         }
@@ -54,7 +63,7 @@ public class Ex1 {
             return false;
         }
         // here we check if the input itself isn't just b since that's my index
-        if (Input.equals("b")){
+        if (Input.equals("b")) {
             return false;
         }
         int indexB = Input.indexOf('b');
@@ -65,8 +74,8 @@ public class Ex1 {
 
             //this loop goes through the number part and checks if the char
             //at place I is a digit or not if not continue to the next char
-            for (int i = 0; i < numberPart.length(); i++){
-                if (!Character.isDigit(numberPart.charAt(i))){
+            for (int i = 0; i < numberPart.length(); i++) {
+                if (!Character.isDigit(numberPart.charAt(i))) {
                     return false;
                 }
             }
@@ -74,17 +83,17 @@ public class Ex1 {
             return true;
         } else {
             // checks the string to make sure the first and last chars are not b
-            if (indexB == 0 || indexB == Input.length() -1 ){
+            if (indexB == 0 || indexB == Input.length() - 1) {
                 return false;
             }
             // now we split the String into number part and base part
-            String numberPart = Input.substring(0,indexB);
-            String basePart = Input.substring(indexB+ 1);
+            String numberPart = Input.substring(0, indexB);
+            String basePart = Input.substring(indexB + 1);
 
             // here we validate the number part that it contain only digits or A-G
-            for (int i = 0; i < numberPart.length(); i++){
+            for (int i = 0; i < numberPart.length(); i++) {
                 char ch = numberPart.charAt(i);
-                if (!Character.isDigit(ch) && (ch < 'A' || ch > 'G')){
+                if (!Character.isDigit(ch) && (ch < 'A' || ch > 'G')) {
                     return false;
                 }
             }
@@ -97,16 +106,16 @@ public class Ex1 {
 
             // here we validate that the number part is valid for the given base
             int base = Character.digit(baseChar, 17);
-            for (int i = 0; i < numberPart.length(); i++){
+            for (int i = 0; i < numberPart.length(); i++) {
                 char ch = numberPart.charAt(i);
 
                 // of the base is less than 10 only digits from 0 to base - 1 are valid
-                if (base <= 10 && (ch < '0' || ch >= ('0' + base))){
+                if (base <= 10 && (ch < '0' || ch >= ('0' + base))) {
                     return false;
                 }
 
                 // if the base is greater than 10 only allow digits 0-9 and A - G (base - 10)
-                if (base > 10 && !(Character.isDigit(ch) || ch >= 'A' && ch < 'A' + (base - 10 ))){
+                if (base > 10 && !(Character.isDigit(ch) || ch >= 'A' && ch < 'A' + (base - 10))) {
                     return false;
                 }
             }
@@ -120,18 +129,18 @@ public class Ex1 {
         String ans = "";
 
         // here we check if the input is valid and if not we return an empty string
-        if (num < 0 || base < 2 || base > 16 ){
+        if (num < 0 || base < 2 || base > 16) {
             return ans;
         }
 
         // here we add an edge case for 0 since 0 stays the same in any base
-        if (num == 0){
+        if (num == 0) {
             return "0";
         }
 
         // here we convert from decimal to the wanted base
         StringBuilder result = new StringBuilder();
-        while ( num > 0) {
+        while (num > 0) {
             int remainder = num % base;
             result.insert(0, remainder);
             num = num / base;
@@ -139,8 +148,8 @@ public class Ex1 {
 
         // after blood sweat and tears (many tears) a function that converts the base part into letters
         char baseRepresentation = base <= 9
-            ? (char)('0' + base )
-            : (char)('A' + base - 10);
+                ? (char) ('0' + base)
+                : (char) ('A' + base - 10);
 
         return result.toString() + "b" + baseRepresentation; // added b and base to the string to show every parsed number as base 10
 
@@ -152,17 +161,34 @@ public class Ex1 {
 
         int n1Int = number2Int(n1);
         int n2Int = number2Int(n2);
-        if (n1Int == n2Int){
+        if (n1Int == n2Int) {
             return true;
         }
         return ans;
     }
 
     public static int maxIndex(String[] arr) {
-        int ans = 0;
-        // add your code here
+        int ans = -1;
+        if (arr == null || arr.length == 0) {
+            return ans;
+        }
+        int maxValue = Integer.MIN_VALUE; // we start with our lowest value and work our way up
 
-        ////////////////////
+        for (int i = 0; i < arr.length; i++) {
+            String num = arr[i];
+
+            if (!isNumber(num)){
+                continue;  // making sure the  input is correct
+            }
+            // convert the number to its decimal value
+            int value = number2Int(num);
+
+            // if the value is bigger than maxValue after the number2Int function assign it as the new maxValue
+            if (value > maxValue){
+                maxValue = value;
+                ans = i; // update the index of the maximum value
+            }
+        }
         return ans;
     }
 }
--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 /**
@@ -9,18 +8,59 @@ import java.util.Scanner;
 public class Ex1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String num1 = "", num2="", quit = "quit";
-        while (!num1.equals(quit) && !num2.equals(quit)) {
+        String num1 = "", num2 = "", quit = "quit";
+        while (true) {
             System.out.println();
             System.out.println("Ex1 class solution:");
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
-            num1 = sc.next();
-            if (!num1.equals("quit")) {
-                // add your code here
 
-                /////////////////////
+            // getting and validating num1
+            num1 = sc.next();
+            if (num1.equals("quit")) {
+                System.out.println("quitting now...");
+                System.exit(0); // Terminates the program
             }
+
+            if (!Ex1.isNumber(num1)){
+                System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
+                continue;
+            }
+            // if num1 is valid run it through number to int and save its value
+            int value1 = Ex1.number2Int(num1);
+
+            // getting num2
+            System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
+            num2 = sc.next();
+            if (num2.equals("quit")) {
+                System.out.println("quiting now...");
+                System.exit(0);
+            }
+            if (!Ex1.isNumber(num2)){
+                System.out.println("ERR: num2 is in the wrong format! ("+num2+")");
+                continue;
+            }
+            // if num2 is valid run it through number to int and save its value
+            int value2 = Ex1.number2Int(num2);
+
+            // getting the output base
+            System.out.println("Enter a base for output (a number between [2,16]): ");
+            int outPutBase = sc.nextInt();
+            if (outPutBase < 2 || outPutBase > 16){
+                System.out.println("ERR: Invalid base!");
+                continue;
+            }
+            // preforming operations
+            int addSum = value1 + value2;
+            int multSum = value1 * value2;
+
+            String addSumStr = Ex1.int2Number(addSum , outPutBase);
+            String multSumStr = Ex1.int2Number(multSum, outPutBase);
+            System.out.println(num1 + " + " + num2 + " = " + addSumStr);
+            System.out.println(num1 + " * " + num2 + " = " + multSumStr);
+
+            String[] results = {num1 , num2 , addSumStr, multSumStr};
+            int maxIdx = Ex1.maxIndex(results);
+            System.out.println("Max number over " + java.util.Arrays.toString(results));
         }
-        System.out.println("quiting now...");
     }
 }
